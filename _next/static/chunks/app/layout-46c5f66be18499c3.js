c090e9b3a60aa8a0a513f918f1fa96434ff8e267
CarlosArrieta <carrieta@defontana.com>
2023-07-26 22:57:40 -0300

    template

--- _next/static/chunks/app/layout-46c5f66be18499c3.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6176:function(e,t,r){Promise.resolve().then(r.bind(r,5939)),Promise.resolve().then(r.t.bind(r,5214,23)),Promise.resolve().then(r.t.bind(r,4648,23))},5939:function(e,t,r){"use strict";r.r(t),r.d(t,{LinguaStateContext:function(){return s},LinguaStateProvider:function(){return u}});var n=r(9268),o=r(6006);let s=(0,o.createContext)({value:"",setValue:()=>{}}),u=e=>{let{children:t}=e,[r,u]=(0,o.useState)("pt");return(0,n.jsx)(s.Provider,{value:{value:r,setValue:u},children:t})}},4648:function(){},5214:function(e){e.exports={style:{fontFamily:"'__Inter_0ec1f4', '__Inter_Fallback_0ec1f4'",fontStyle:"normal"},className:"__className_0ec1f4"}},3177:function(e,t,r){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=r(6006),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,a=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function l(e,t,r){var n,s={},l=null,f=null;for(n in void 0!==r&&(l=""+r),void 0!==t.key&&(l=""+t.key),void 0!==t.ref&&(f=t.ref),t)u.call(t,n)&&!i.hasOwnProperty(n)&&(s[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===s[n]&&(s[n]=t[n]);return{$$typeof:o,type:e,key:l,ref:f,props:s,_owner:a.current}}t.Fragment=s,t.jsx=l,t.jsxs=l},9268:function(e,t,r){"use strict";e.exports=r(3177)}},function(e){e.O(0,[253,769,744],function(){return e(e.s=6176)}),_N_E=e.O()}]);
