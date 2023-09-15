2eb26c9b023be258dbee2c1a927b0efd71f56917
CarlosArrieta <carrieta@defontana.com>
2023-09-15 15:46:42 -0300

    add blog markdown parser

---
+++ _next/static/wHealsxnSfg_R9pWNvuNV/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fblog\u002Fpage\u002F[slug]","\u002Fcursos\u002F[slug]","\u002Fprofessores\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
