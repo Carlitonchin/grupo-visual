ec2250883585dcd31065a59604cd0a4a6aa362dd
CarlosArrieta <carrieta@defontana.com>
2023-10-17 13:51:12 -0300

    change card promo text

---
+++ _next/static/chunks/app/blog/[slug]/page-0b9c58b34c33bfa8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[308],{6121:function(n,e,u){Promise.resolve().then(u.t.bind(u,2569,23))},2569:function(){}},function(n){n.O(0,[971,864,744],function(){return n(n.s=6121)}),_N_E=n.O()}]);
